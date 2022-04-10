ea4c5188a5165a877102e71015a0e7c58f74729a
Shin-sibainu <shincode0712@gmail.com>
2022-04-10 10:38:11 +0900

    register

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  Redirect,
+  Navigate,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { user } = useContext(AuthContext);
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
         <Route path="/profile/:username" element={<Profile />} />
       </Routes>
     </Router>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Register.css";
 
 export default function Register() {
+  const username = useRef();
+  const email = useRef();
+  const password = useRef();
+  const passwordConfirmation = useRef();
+
+  const navigate = useNavigate();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    // console.log(password.current.value);
+    // console.log(passwordConfirmation.current.value);
+
+    //パスワードと確認用パスワードが合っているかどうか確認
+    if (password.current.value !== passwordConfirmation.current.value) {
+      passwordConfirmation.current.setCustomValidity("パスワード違います");
+    } else {
+      try {
+        const user = {
+          username: username.current.value,
+          email: email.current.value,
+          password: password.current.value,
+        };
+        //registerAPIを叩く
+        await axios.post("/auth/register", user);
+        navigate("/login");
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -10,27 +43,43 @@ export default function Register() {
           <span className="loginDesc">本格的なSNSを、自分の手で。</span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
+          <form className="loginBox" onSubmit={(e) => handleClick(e)}>
             <p className="loginMsg">新規登録はこちら</p>
             <input
               type="text"
               className="loginInput"
               placeholder="ユーザー名"
+              required
+              ref={username}
             />
-            <input type="text" className="loginInput" placeholder="Eメール" />
             <input
-              type="text"
+              type="email"
+              className="loginInput"
+              placeholder="Eメール"
+              required
+              ref={email}
+            />
+            <input
+              type="password"
               className="loginInput"
               placeholder="パスワード"
+              required
+              minLength="6"
+              ref={password}
             />
             <input
-              type="text"
+              type="password"
               className="loginInput"
               placeholder="確認用パスワード"
+              required
+              minLength="6"
+              ref={passwordConfirmation}
             />
-            <button className="loginButton">サインアップ</button>
+            <button className="loginButton" type="submit">
+              サインアップ
+            </button>
             <button className="loginRegisterButton">ログイン</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
