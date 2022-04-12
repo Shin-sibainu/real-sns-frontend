136fc2c595c4bef89205112489c37843a0a7c109
Shin-sibainu <shincode0712@gmail.com>
2022-04-12 20:43:38 +0900

    mod

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -17,8 +17,11 @@ export default function Feed({ username }) {
         ? await axios.get(`/posts/profile/${username}`) //プロフィールの場合
         : await axios.get(`/posts/timeline/${user._id}`); //ホームの場合
 
-      setPosts(res.data);
-      // console.log(res.data);
+      setPosts(
+        res.data.sort((p1, p2) => {
+          return new Date(p2.createdAt) - new Date(p1.createdAt);
+        })
+      );
     };
     fetchPosts();
   }, [username]);
