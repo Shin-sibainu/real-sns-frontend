14e15f37eed409b4f6c23710a26c271995060419
Shin-sibainu <shincode0712@gmail.com>
2022-06-01 07:32:20 +0900

    localstorage for user

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,20 +1,21 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
 //最初のユーザー状態の定義
 const initialState = {
   // user: null, //ログインしてないですね。
-  user: {
-    _id: "62301ab54ded0ed0584598e4",
-    username: "shincode",
-    email: "shincode@gmail.com",
-    password: "abcdefg",
-    profilePicture: "/person/1.jpeg",
-    coverPicture: "",
-    followers: [],
-    followings: [],
-    isAdmin: false,
-  },
+  // user: {
+  //   _id: "62301ab54ded0ed0584598e4",
+  //   username: "shincode",
+  //   email: "shincode@gmail.com",
+  //   password: "abcdefg",
+  //   profilePicture: "/person/1.jpeg",
+  //   coverPicture: "",
+  //   followers: [],
+  //   followings: [],
+  //   isAdmin: false,
+  // },
+  user: JSON.parse(localStorage.getItem("user")) || null,
   isFetching: false, //ログインしようともしてないですね。
   error: false, //エラーも吐いてないですね。
 };
@@ -24,6 +25,12 @@ export const AuthContext = createContext(initialState);
 export const AuthContextProvider = ({ children }) => {
   //ユーザー入力によって状態(state)が更新され、それをグローバルに利用している。
   const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+  //set user data in localstroge
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
+
   return (
     <AuthContext.Provider
       value={{
