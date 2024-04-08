26ef85cba76c8f80daf3eecf2cab004a58ce3615
Shin-sibainu <71575781+Shin-sibainu@users.noreply.github.com>
2024-04-08 14:59:27 +0900

    Update apiCalls.js

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,12 @@
 import axios from "axios";
+import { LoginStart, LoginSuccess, LoginError } from "../apiCall.js"
 
 export const loginCall = async (user, dispatch) => {
-  dispatch({ type: "LOGIN_START" });
+  dispatch({ type: "LOGIN_START" }); // dispatch(LoginStart(user)) こちらでも可です。
   try {
     const res = await axios.post("auth/login", user);
-    dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    dispatch({ type: "LOGIN_SUCCESS", payload: res.data }); // dispatch(LoginSuccess(user)) こちらでも可です。
   } catch (err) {
-    dispatch({ type: "LOGIN_ERROR", payload: err });
+    dispatch({ type: "LOGIN_ERROR", payload: err }); // dispatch(LoginError(err)) こちらでも可です。
   }
 };
